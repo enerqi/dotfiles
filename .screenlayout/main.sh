4d4a01d202ffe9d36317daabf2a61fb69a8564bb
enerqi <enerqi@github>
2018-01-05 11:52:58 +0000

    Ubuntu 17 updates + i3 screen layout files.

---
+++ .screenlayout/main.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --dpi 128 --output DP-1-3 --mode 3840x2160 --pos 1920x0 --rotate normal --output HDMI-1-3 --off --output HDMI-1-2 --off --output HDMI-1-1 --mode 1920x1080 --pos 5760x520 --rotate normal --output DP-1-2 --off --output eDP-1-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-1 --off --output DP-3 --off --output DP-2 --off --output DP-1-1 --off --output DP-0 --off
