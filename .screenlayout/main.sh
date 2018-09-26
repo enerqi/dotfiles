59ba865901ca2ebebfbdadc4220271102e25bb5a
kelvin.ward <kelvin.ward@gigaclear.com>
2018-09-26 16:55:55 +0100

    Main screen layout tweak.

--- .screenlayout/main.sh
+++ .screenlayout/main.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --dpi 128 --output DP-1-3 --mode 3840x2160 --pos 1920x0 --rotate normal --output HDMI-1-3 --off --output HDMI-1-2 --off --output HDMI-1-1 --mode 1920x1080 --pos 5760x520 --rotate normal --output DP-1-2 --off --output eDP-1-1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-1 --off --output DP-3 --off --output DP-2 --off --output DP-1-1 --off --output DP-0 --off
+xrandr --output DP-1-3 --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-1-3 --off --output HDMI-1-2 --off --output HDMI-1-1 --mode 1920x1080 --pos 3840x144 --rotate left --output DP-1-2 --off --output eDP-1-1 --primary --mode 1920x1080 --pos 4920x984 --rotate normal --output DP-1 --off --output DP-3 --off --output DP-2 --off --output DP-1-1 --off --output DP-0 --off
