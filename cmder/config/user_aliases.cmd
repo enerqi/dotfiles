475fb196f6f6da0ff2dcc73d326ca1f6ef7db77c
enerqi <enerqi>
2022-01-01 21:53:41 +0000

    Cmder cd alias to zcd for zoxide ergonomics

--- cmder/config/user_aliases.cmd
+++ cmder/config/user_aliases.cmd
@@ -4,9 +4,11 @@
 ;= rem In batch mode, jump to the end of the file
 ;= goto:eof
 ;= Add aliases below here
+;= https://github.com/cmderdev/cmder/wiki/Cmder-Aliases
 e.=explorer .
 gl=git log --oneline --all --graph --decorate  $*
 ls=ls --show-control-chars -F --color $*
+cd=zcd $*
 pwd=cd
 clear=cls
 history=cat %CMDER_ROOT%\config\.history
@@ -15,7 +17,9 @@ vi=vim $*
 cmderr=cd /d "%CMDER_ROOT%"
 gst=git status $*
 l=ls --show-control-chars -F --color -Alh $*
+lt=ls -lstr --color=auto
 gfm=git pull $*
 udesql=pclip | grep -v === | column -t
 duplicateLines=sed "/^$/d" $1 | sort | uniq -d
 pkt=.paket\paket.exe $* 
+reload=%CMDER_ROOT%\vendor\init.bat
