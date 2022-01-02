8ec36b17e38441b76be00006b651c667475fb35b
enerqi <enerqi@enerqi>
2022-01-02 14:39:03 +0000

    fetch-fonts.sh utility for nerdfonts

---
+++ bin/fetch-fonts.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# From github - https://www.nerdfonts.com/font-downloads
+# Update release version after checking https://github.com/ryanoasis/nerd-fonts/releases/
+
+declare -a fonts=(
+    BitstreamVeraSansMono
+    CascadiaCode
+    CodeNewRoman
+    DroidSansMono
+    FiraCode
+    FiraMono
+    Go-Mono
+    Hack
+    Hermit
+    Inconsolata
+    JetBrainsMono
+    Meslo
+    Noto
+    Overpass
+    ProggyClean
+    RobotoMono
+    SourceCodePro
+    SpaceMono
+    Ubuntu
+    UbuntuMono
+)
+
+version='2.1.0'
+fonts_dir="${HOME}/.local/share/fonts"
+
+if [[ ! -d "$fonts_dir" ]]; then
+    mkdir -p "$fonts_dir"
+fi
+
+for font in "${fonts[@]}"; do
+    zip_file="${font}.zip"
+    download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/v${version}/${zip_file}"
+    echo "Downloading $download_url"
+    wget "$download_url"
+    unzip "$zip_file" -d "$fonts_dir"
+    rm "$zip_file"
+done
+
+find "$fonts_dir" -name '*Windows Compatible*' -delete
+
+fc-cache -fv
