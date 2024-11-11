f030ef0f1af134f853785b9a4da01d95a305eaef
Enerqi <>
2024-11-11 13:05:14 +0000

    wezterm right status. Linux customization.

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,9 +1,10 @@
 -- examples
 -- https://alexplescan.com/posts/2024/08/10/wezterm/#multiplexing-terminals-levelling-up-key-assignments
 
--- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require 'wezterm'  -- Pull in the wezterm API
 local act = wezterm.action
+local config = wezterm.config_builder()  -- This will hold the configuration.
+local is_windows_os = wezterm.target_triple == 'x86_64-pc-windows-msvc'
 
 -- Debugging (ctrl shift L), ctrl D to exit etc.
 -- myconfig = window:effective_config()
@@ -16,16 +17,15 @@ function get_keys(t)
   return keys
 end
 
-
-
--- This will hold the configuration.
-local config = wezterm.config_builder()
-
 -- Launch menu options
 local launch_menu = {}
 config.launch_menu = launch_menu
+table.insert(launch_menu, {
+    label = 'Nu',
+    args = { 'nu.exe' },
+})
 
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+if is_windows_os then
 
   -- Add powershell to launch menu
   table.insert(launch_menu, {
@@ -55,37 +55,51 @@ if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
       },
     })
   end
+else
+
+    -- Default to zsh on Posix O.S.
+  config.default_prog = { 'zsh' }
 end
 
-table.insert(launch_menu, {
-    label = 'Nu',
-    args = { 'nu.exe' },
-})
 
 
 -- Appearance settings
 -- note wezterm bundles jetbrains mono and nerd font symbols
 -- `wezterm ls-fonts`, `wezterm ls-fonts --list-system`
 config.window_decorations = "RESIZE"  -- default "TITLE | RESIZE"
-config.use_fancy_tab_bar = true
+config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.color_scheme = 'Monokai (dark) (terminal.sexy)'
-config.window_background_opacity = 0.95
 config.enable_scroll_bar = true
 -- config.background = {source = {File = 'C:/Users/Enerqi/Pictures/chi-20201002-rot.png'}}
 -- C:\Users\Enerqi\Pictures/chi-20201002.jpg
+if is_windows_os then
+  config.font = wezterm.font 'CaskaydiaCove NF Mono'
+  config.window_background_opacity = 0.95
+else
+  config.font = wezterm.font "CaskaydiaCove Nerd Font Mono"
+  config.window_background_opacity = 0.85
+end
 -- config.font = wezterm.font 'FuraCode Nerd Font Mono'
-config.font = wezterm.font 'CaskaydiaCove NF Mono'
 -- config.font = wezterm.font 'Jetbrains Mono'
 -- config.font = wezterm.font('JetBrains Mono', { weight = 'Bold', italic = true })
 config.font_size = 14
 
 function dirname(path_str)
     local rev = path_str:reverse()
-    local start = rev:find("[/]")
-    local endoffset = string.find(rev, "[/]", start+1) --rev:find("/", init=start, plain=true)
-    local rdir = rev:sub(start+1, endoffset-1)
-    return rdir:reverse()
+    if is_windows_os then
+        -- nu windows /foo/bar/baz/  (trailing /)
+        local start = rev:find("[/]")
+        local endoffset = string.find(rev, "[/]", start+1)
+        local rdir = rev:sub(start+1, endoffset-1)
+        return rdir:reverse()
+    else
+        -- zsh /foo/bar/baz
+        local start = 1
+        local endoffset = string.find(rev, "[/]", start+1)
+        local rdir = rev:sub(start, endoffset-1)
+        return rdir:reverse()
+    end
 end
 
 -- This function returns the suggested title for a tab.
@@ -124,6 +138,29 @@ wezterm.on(
     -- end
   end
 )
+
+wezterm.on('update-right-status', function(window, pane)
+  local cwd_uri = pane:get_current_working_dir()
+  local cwd = cwd_uri.file_path
+  local who = os.getenv('USER') or os.getenv('LOGNAME') or os.getenv('USERNAME')
+
+  local hostname = cwd_uri.host or wezterm.hostname()
+  local dot = hostname:find '[.]'
+  if dot then
+    hostname = hostname:sub(1, dot - 1)
+  end
+  if hostname == '' then
+    hostname = wezterm.hostname()
+  end
+
+  window:set_right_status(wezterm.format {
+    { Background = { Color = '#3c1361' } },
+    -- { Attribute = { Bold = true } },
+    { Text = wezterm.nerdfonts.oct_file_directory .. ' ' .. cwd .. ' ' ..
+      wezterm.nerdfonts.fa_user .. ' ' .. who .. ' '  ..
+      wezterm.nerdfonts.md_desktop_classic .. ' ' .. hostname .. ' '},
+  })
+end)
 
 -- Quick Select Mode (ctrl shift space)
 -- quick select mode is used to quickly select and copy commonly used patterns
